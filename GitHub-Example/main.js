adfda3d68ee2c8a87728c79019dc15c900bc2726
abg3000 <abg3000@naver.com>
2021-11-22 22:48:47 +0900

    Responsive commit

---
+++ GitHub-Example/main.js
@@ -0,0 +1,28 @@
+(function (window, document) {
+  "use strict"; //엄격한 자바스크립트 문법으로 시작하겠다는 뜻
+
+  const $toggles = document.querySelectorAll(".toggle"); //토글이란 클래스를 가지고 있는 모든 요소 Nodelist <-유사배열
+  const $toggleBtn = document.getElementById("toggle-btn");
+
+  $toggleBtn.addEventListener("click", function () {
+    toggleElements();
+  }); //클릭 이벤트가 감지되면 함수 실행
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 1024) {
+      offElements();
+    }
+  }); //윈도우에 resize이벤트가 발생할 때, 함수 실행
+
+  function toggleElements() {
+    [].forEach.call($toggles, function (toggle) {
+      toggle.classList.toggle("on"); //실행되면 on이라는 클래스를 붙임
+    }); //유사배열을 진짜 배열과 연결
+  }
+
+  function offElements() {
+    [].forEach.call($toggles, function (toggle) {
+      toggle.classList.remove("on"); //실행되면 on이라는 클래스를 지움
+    }); //유사배열을 진짜 배열과 연결
+  }
+})(window, document); //전체의 영역을 더럽힐 수 있기 때문에 구분화 모듈화
