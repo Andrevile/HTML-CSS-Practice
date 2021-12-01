045916253505a32f71af2dfd4e1c2e28f3ae32f6
abg3000 <abg3000@naver.com>
2021-12-01 17:32:45 +0900

    menu slide

--- GitHub-Example/main.js
+++ GitHub-Example/main.js
@@ -3,7 +3,7 @@
 
   const $toggles = document.querySelectorAll(".toggle"); //토글이란 클래스를 가지고 있는 모든 요소 Nodelist <-유사배열
   const $toggleBtn = document.getElementById("toggle-btn");
-
+  const toggle = document.querySelector(".toggle");
   $toggleBtn.addEventListener("click", function () {
     toggleElements();
   }); //클릭 이벤트가 감지되면 함수 실행
@@ -15,8 +15,23 @@
   }); //윈도우에 resize이벤트가 발생할 때, 함수 실행
 
   function toggleElements() {
+    let totalHeight = 0;
     [].forEach.call($toggles, function (toggle) {
-      toggle.classList.toggle("on"); //실행되면 on이라는 클래스를 붙임
+      console.log(window.getComputedStyle(toggle).maxHeight);
+      const ElementHeight = window.getComputedStyle(toggle).maxHeight;
+      if (ElementHeight !== "0px") {
+        // offElements();
+
+        toggle.style.maxHeight = "0px";
+      } else {
+        toggle.style.maxHeight = toggle.scrollHeight + 10 + "px";
+        // toggle.classList.toggle("on"); //실행되면 on이라는 클래스를 붙임
+        // console.log(toggle.style);
+      }
+      // const prevHeight = toggle.style;
+      // console.log(getComputedStyle(toggle).getPropertyValue("max-height"));
+      // console.log(this);
+      // console.log(toggle.classList);
     }); //유사배열을 진짜 배열과 연결
   }
 
